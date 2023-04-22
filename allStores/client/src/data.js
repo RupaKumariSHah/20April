5562582f28f4b03e856426ffc054c36126f0009c
Siya <RupaShah9055@gmail.com>
2023-04-22 17:22:02 +0530

    addapp

---
+++ allStores/client/src/data.js
@@ -0,0 +1,91 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/XsdmR2c/1.png",
+    title: "Comming Soon SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://media.istockphoto.com/id/1412353022/photo/empty-aisle-at-a-supermarket.jpg?s=1024x1024&w=is&k=20&c=pGJwrvfeoDDxEJKBvIqiomuXUlr9U5ce5HDyolukK_E=",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    // img: "https://i.ibb.co/cXFnLLV/3.png",
+    img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Frice-fields&psig=AOvVaw28dwNAWw7ijGRk3zQ7RIL4&ust=1682222019458000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJjxyM7LvP4CFQAAAAAdAAAAABAD",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    // img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+    img: "https://images.pexels.com/photos/7421198/pexels-photo-7421198.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Different Rice TYPES!",
+    // cat: "Grains-Types/Rice ",
+    cat: "Grains",
+  },
+  {
+    id: 2,
+    // img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://media.istockphoto.com/id/1324180630/photo/macro-close-up-of-organic-yellow-gram-or-split-yellow-moong-dal-inside-a-ceramic-white-soup.jpg?s=1024x1024&w=is&k=20&c=uXej11n3DrihNkWVXC731VSlrQxIX0tuJBtcZRF2YVY=",
+    title: "Pulses TYPES",
+    // cat: "pulses/daal-Types",
+    cat: "pulses",
+  },
+  {
+    id: 3,
+    // img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    img: "https://images.pexels.com/photos/5966434/pexels-photo-5966434.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Dry Beans TYPES",
+    // cat:"jeans"
+    // cat: "dry-beans(moomg,pea...)",
+    cat: "dry-beans",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    // img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    img:"https://www.pexels.com/photo/close-up-photo-of-assorted-rice-4110255/",
+  },
+  {
+    id: 2,
+    // img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    img:"https://www.pexels.com/photo/close-up-photo-of-rice-on-person-s-hand-4110256/",
+  },
+  {
+    id: 3,
+    // img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    img:"https://www.pexels.com/photo/cereal-in-bowl-14637618/",
+  },
+  {
+    id: 4,
+    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+];
---
+++ allStores/client/src/pages/ProductList.jsx
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+import { mobile } from "../responsive";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+  ${mobile({ marginRight: "0px" })}
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+  ${mobile({ margin: "10px 0px" })}
+`;
+const Option = styled.option``;
+
+const ProductList = () => {
+  const location = useLocation();
+  console.log(location.pathname.split("/")[2]);
+  const cat = location.pathname.split("/")[2];
+  const [filters, setFilters] = useState({}); //select the quantity
+  const [sort, setSort] = useState("newest");
+
+  const handleFilters = (e) => {
+    const value = e.target.value;
+    setFilters({
+      ...filters,
+      [e.target.name]: value,
+    });
+  };
+  console.log(filters);
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>{cat}</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select name="color" onChange={handleFilters}>
+            <Option disabled>Color</Option>
+            <Option>whiteRice</Option>
+            <Option>BrownRice</Option>
+            <Option>chana daal</Option>
+            <Option>moong daal </Option>
+            <Option>greengram</Option>
+            <Option>Sabbot massori</Option>
+          </Select>
+          <Select name="size" onChange={handleFilters}>
+            <Option disabled>Size</Option>
+            <Option>1</Option>
+            <Option>2</Option>
+            <Option>3</Option>
+            <Option>4</Option>
+            <Option>5</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select onChange={(e) => setSort(e.target.value)}>
+            <Option value="newest">Newest</Option>
+            <Option value="asc">Price (asc)</Option>
+            <Option value="desc">Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products cat={cat} filters={filters} sort={sort} />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+//filter,catageory ,products
+export default ProductList;
